1cbc7ce594def2b7b62a82a1d07510df65a160bf
sunlight <95218963+sunlightx@users.noreply.github.com>
2024-07-18 19:37:55 +0300

    Delete surface-area-and-volume-box.js

--- surface-area-and-volume-box.js
+++
@@ -1,8 +0,0 @@
-// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
-
-// My solution
-function getSize(width, height, depth) {
-	const volume = width * height * depth
-	const area = 2 * (width * height) + 2 * (width * depth) + 2 * (depth * height)
-	return [area, volume]
-}
