fa14b459bb402d383972c718e4c12b164ba86aef
unknown <sliepkov.1998@gmail.com>
2024-07-18 20:12:58 +0300

    switcher

--- switch-up.js
+++ switch-up.js
@@ -5,28 +5,17 @@
 // Output :: "One".
 
 function switchItUp(number) {
-	switch (number) {
-		case 0:
-			return 'Zero'
-		case 1:
-			return 'One'
-		case 2:
-			return 'Two'
-		case 3:
-			return 'Three'
-		case 4:
-			return 'Four'
-		case 5:
-			return 'Five'
-		case 6:
-			return 'Six'
-		case 7:
-			return 'Seven'
-		case 8:
-			return 'Eight'
-		case 9:
-			return 'Nine'
-		default:
-			return '0-9'
+	const switcher = {
+		0: 'Zero',
+		1: 'One',
+		2: 'Two',
+		3: 'Three',
+		4: 'Four',
+		5: 'Five',
+		6: 'Six',
+		7: 'Seven',
+		8: 'Eight',
+		9: 'Nine',
 	}
+	return switcher[number]
 }
